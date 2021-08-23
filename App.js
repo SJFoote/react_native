5a4639043eb182c929b7536ebc48cb0980ed0a9a
Spencer Foote <83970495+SJFoote@users.noreply.github.com>
2021-08-23 11:02:01 -0700

    update

--- App.js
+++ App.js
@@ -1,12 +1,40 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image, Button, TextInput, ScrollView } from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    <View style={{flex: 1, justifyContent: 'center', alignContent: 'center'}}>
+        <View style={{ 
+          width: 100, 
+          height: 100, 
+          backgroundColor:'red'
+  }}/>
+    <View style={{ 
+      width: 100, 
+      height: 100, 
+      backgroundColor:'blue'
+  }}/>
+      <View style={{ 
+        width: 100, 
+        height: 100, 
+        backgroundColor:'green'
+  }}/>
+        <View style={{ 
+          width: 100, 
+          height: 100, 
+          backgroundColor:'yellow'
+  }}/>
+      <Text>Hello World!
       <StatusBar style="auto" />
+      </Text>
+      <Text>My name is Spencer
+      <StatusBar style="auto" />
+      </Text>
+      <Text>This is my first react native app!
+      <StatusBar style="auto" />
+      </Text>
+
     </View>
   );
 }
